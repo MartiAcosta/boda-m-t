457a80353e3353b4ce8f03e89f80c7e657d9ef23
MartiAcosta <martinaacosta17@gmail.com>
2023-10-18 21:42:42 -0300

    Modal confirmacion

--- src/modal.js
+++ src/modal.js
@@ -13,11 +13,13 @@ function ConfirmarInvitados({ onClose }) {
         menu: '',
     })
 
+    const [confirmationDisplayed, setConfirmationDisplayed] = useState(false);
 
     const handleConfirInvitados = async () => {
         try {
             const response = await axios.post('http://localhost:3001/invitados', invitadosData);
             console.log('Invitados creado:', response.data)
+            setConfirmationDisplayed(true);
         } catch (error) {
             console.log('Error al crear invitado', error);
         }
@@ -28,85 +30,94 @@ function ConfirmarInvitados({ onClose }) {
             <div className="modal">
                 <div className="modal-content">
                     <button type="button" className="close-button" onClick={onClose}>X</button>
-
-                    <div className="intro-section">
-                        <p>Estamos emocionados de compartir este día especial con</p>
-                        <p>personas tan importantes para nosotros.</p>
-                        <p>Dado el formato exclusivo de nuestra celebración, hemos</p>
-                        <p>optado por una contribución por asistencia para cubrir los</p>
-                        <p>costos del evento.</p>
-                    </div>
-
-                    <div className="bank-details">
-                        <p>Contribución por asistencia:</p>
-                        <p style={{ fontWeight: 600, fontSize:'20px', marginTop: '2px' }}>$26600</p>
-                        <p>Datos de la Cuenta Bancaria:</p>
-                        <p style={{ fontWeight: 600, fontSize:'20px', marginTop: '2px' }}>5566 5687 5464 6456</p>
-                    </div>
-                        <div className="confirm-details">
-                            <p>Confirma tu asistencia</p>
+                    {confirmationDisplayed ? (
+                        <div className="confirmation-message">
+                            <p style={{fontFamily: 'montigny', fontSize: '35px'}}>Gracias</p>
+                            <p>Te esperamos para celebrar</p>
                         </div>
-                        <form onSubmit={e => e.preventDefault()} className='form-container'>
-                            <div className="form-group">
-                                <input
-                                    type="text"
-                                    id="nombre"
-                                    name='nombre'
-                                    placeholder='Nombre'
-                                    value={invitadosData.nombre}
-                                    onChange={e => setInvitadosData({ ...invitadosData, nombre: e.target.value })}
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <input
-                                    type="text"
-                                    id="apellido"
-                                    placeholder="Apellido" 
-                                    value={invitadosData.apellido}
-                                    onChange={e => setInvitadosData({ ...invitadosData, apellido: e.target.value })}
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <input
-                                    type="text"
-                                    id="codigo"
-                                    placeholder='Codigo de area'
-                                    value={invitadosData.dni}
-                                    onChange={e => setInvitadosData({ ...invitadosData, cod: e.target.value })}
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <input
-                                    type="text"
-                                    id="telefono"
-                                    placeholder='Telefono'
-                                    value={invitadosData.tel}
-                                    onChange={e => setInvitadosData({ ...invitadosData, tel: e.target.value })}
-                                />
+                    ) : (
+                        <div>
+                            <div className="intro-section">
+                                <p>Estamos emocionados de compartir este día especial con</p>
+                                <p>personas tan importantes para nosotros.</p>
+                                <p>Dado el formato exclusivo de nuestra celebración, hemos</p>
+                                <p>optado por una contribución por asistencia para cubrir los</p>
+                                <p>costos del evento.</p>
                             </div>
 
                             <div className="bank-details">
-                                <p>Queremos asegurarnos de que todos nuestros invitados</p>
-                                <p>disfruten de la comida en nuestro gran día. Si tienes alguna</p> 
-                                <p>preferencia dietética, por favor seleccione una opción:</p>
-                                <select
-                                    type="text"
-                                    id="menu"
-                                    value={invitadosData.menu}
-                                    onChange={e => setInvitadosData({ ...invitadosData, menu: e.target.value })}
-                                    >
-                                    <option value="">Selecciona menú</option>
-                                    <option value="celiaco">Celíaco</option>
-                                    <option value="vegano">Vegano</option>
-                                    <option value="vegetariano">Vegetariano</option>
-                                    <option value="sinRestricciones">Sin restricciones</option>
-                                </select>
+                                <p>Contribución por asistencia:</p>
+                                <p style={{ fontWeight: 600, fontSize: '20px', marginTop: '2px' }}>$26600</p>
+                                <p>Datos de la Cuenta Bancaria:</p>
+                                <p style={{ fontWeight: 600, fontSize: '20px', marginTop: '2px' }}>5566 5687 5464 6456</p>
                             </div>
-                        </form>
-                        <button className='bottomConfir' type="button" onClick={handleConfirInvitados}>Confirmar</button>
+                            <div className="confirm-details">
+                                <p>Confirma tu asistencia</p>
+                            </div>
+                            <form onSubmit={e => e.preventDefault()} className='form-container'>
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        id="nombre"
+                                        name='nombre'
+                                        placeholder='Nombre'
+                                        value={invitadosData.nombre}
+                                        onChange={e => setInvitadosData({ ...invitadosData, nombre: e.target.value })}
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        id="apellido"
+                                        placeholder="Apellido"
+                                        value={invitadosData.apellido}
+                                        onChange={e => setInvitadosData({ ...invitadosData, apellido: e.target.value })}
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        id="codigo"
+                                        placeholder='Codigo de area'
+                                        value={invitadosData.dni}
+                                        onChange={e => setInvitadosData({ ...invitadosData, cod: e.target.value })}
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        id="telefono"
+                                        placeholder='Telefono'
+                                        value={invitadosData.tel}
+                                        onChange={e => setInvitadosData({ ...invitadosData, tel: e.target.value })}
+                                    />
+                                </div>
+
+                                <div className="bank-details">
+                                    <p>Queremos asegurarnos de que todos nuestros invitados</p>
+                                    <p>disfruten de la comida en nuestro gran día. Si tienes alguna</p>
+                                    <p>preferencia dietética, por favor seleccione una opción:</p>
+                                    <select
+                                        type="text"
+                                        id="menu"
+                                        value={invitadosData.menu}
+                                        onChange={e => setInvitadosData({ ...invitadosData, menu: e.target.value })}
+                                    >
+                                        <option value="">Selecciona menú</option>
+                                        <option value="celiaco">Celíaco</option>
+                                        <option value="vegano">Vegano</option>
+                                        <option value="vegetariano">Vegetariano</option>
+                                        <option value="sinRestricciones">Sin restricciones</option>
+                                    </select>
+                                </div>
+                            </form>
+                            <button className='bottomConfir' type="button" onClick={handleConfirInvitados}>Confirmar</button>
+                        </div>
+
+                    )}
                 </div>
             </div>
         </div>,
